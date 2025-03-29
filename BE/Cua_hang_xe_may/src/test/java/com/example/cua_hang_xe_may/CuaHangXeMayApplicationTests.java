bffd29f6b122ef6755b13424dd82d202314e23b2
Trung Tran <21130590@st.hcmuaf.edu.vn>
2025-03-30 00:51:41 +0700

    project

---
+++ BE/Cua_hang_xe_may/src/test/java/com/example/cua_hang_xe_may/CuaHangXeMayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cua_hang_xe_may;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuaHangXeMayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
